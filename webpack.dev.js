33c5881ab3d25b22bc1198bc355bb288e39ab798
benjamin <d400801@gmail.com>
2018-12-08 19:31:07 +0200

    refactoring webpack files

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,9 +1,6 @@
 const merge = require('webpack-merge');
 const webpack = require('webpack');
 const common = require('./webpack.common.js');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-
 
 module.exports = merge(common, {
     mode: 'development',
@@ -12,58 +9,6 @@ module.exports = merge(common, {
         contentBase: './dist'
     },
     plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'test.html',
-            template: './src/test.html',
-        }),
-        new webpack.HotModuleReplacementPlugin(),
-        new ExtractTextPlugin({ filename: 'style.css' })
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            },
-            {
-                test: /\.scss$/,
-                use: ExtractTextPlugin.extract(
-                    {
-                        fallback: 'style-loader',
-                        use: [
-                            {
-                                loader: 'css-loader',
-                                options: {
-                                    minimize: true || {/* CSSNano Options */ }
-                                }
-                            },
-                            {
-                                loader: 'postcss-loader'
-                            },
-                            {
-                                loader: 'sass-loader'
-                            }
-                        ]
-                    })
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    'file-loader'
-                ]
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: [
-                    'file-loader'
-                ]
-            }
-        ]
-    }
+        new webpack.HotModuleReplacementPlugin()
+    ]
 });
