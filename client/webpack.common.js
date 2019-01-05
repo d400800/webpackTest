a60a8a43b62e689af580e11016a4ab71023dee13
benjamin <d400801@gmail.com>
2019-01-05 14:01:08 +0200

    making client and server, adding bootstrap

---
+++ client/webpack.common.js
@@ -0,0 +1,75 @@
+const path = require('path');
+const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+    css: './src/scss/main.scss',
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery"
+    }),
+    //new CleanWebpackPlugin(['dist']),
+    // new HtmlWebpackPlugin({
+    //   template: './src/index.html'
+    // }),
+    // new HtmlWebpackPlugin({
+    //   filename: 'test.html',
+    //   template: './src/test.html',
+    // }),
+    new ExtractTextPlugin({ filename: 'style.css' })
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, '../server/static/dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract(
+          {
+            fallback: 'style-loader',
+            use: [
+              {
+                loader: 'css-loader',
+                options: {
+                  minimize: true || {/* CSSNano Options */ }
+                }
+              },
+              {
+                loader: 'postcss-loader'
+              },
+              {
+                loader: 'sass-loader'
+              }
+            ]
+          })
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  }
+};
